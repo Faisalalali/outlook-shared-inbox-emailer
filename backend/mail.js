bf8344a37cb3730f2c754ec8c627ba34ace549b7
Faisal Alali <30318708+Faisalalali@users.noreply.github.com>
2023-02-25 19:02:51 +0300

    added bodyParser & fixed `npm run dev`

--- backend/mail.js
+++ backend/mail.js
@@ -2,11 +2,15 @@ const nodemailer = require("nodemailer");
 const dotenv = require("dotenv");
 
 dotenv.config();
-console.log(process.env.EMAIL_SERVICE);
 function sendMail(req, res) {
-    const { to, subject, message } = req.body;
-    console.log(process.env.EMAIL_SERVICE);
+    const { to, subject, message, html, cc, bcc } = req.body;
 
+    // Make sure that either message or html is present
+    if (!message && !html) {
+        return res.status(400).json({ error: 'Either message or html must be present' });
+    }
+
+    // Set up the transporter
     const transporter = nodemailer.createTransport({
         service: process.env.EMAIL_SERVICE,
         auth: {
@@ -15,19 +19,26 @@ function sendMail(req, res) {
         }
     });
 
+    // Set up the email options
     const options = {
         from: process.env.EMAIL_SENDER,
-        to,
-        subject,
-        html: message,
+        to: to,
+        subject: subject
     };
+
+    // If html is present, use it, otherwise use message
+    if (html) { options.html = html; }
+    else { options.message = message; }
+
+    // If cc or bcc are present, add them to the options
+    if (cc) { options.cc = cc; }
+    if (bcc) { options.bcc = bcc; }
 
     transporter.sendMail(options, (error, info) => {
         if (error) {
-            console.log(error);
             res.status(500).json({ message: 'An error occurred while sending the email.' });
         } else {
-            console.log(info);
+            console.log('Email sent: ' + info.response);
             res.json({ message: 'Email sent successfully.' });
         }
     });
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,13 @@
 const express = require('express');
-const sendMail = require('../mail');
+const bodyParser = require('body-parser');
+
+const sendMail = require('./mail');
 
 const app = express();
+
+// parse incoming request bodies
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.post('/send-mail', (req, res) => {
     sendMail(req, res);
